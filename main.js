78ab49075d74c0c08bd615aad403ad93318472e4
Jackof2002 <131018814+Jackof2002@users.noreply.github.com>
2026-02-18 17:37:31 -0500

    FincaHotelHeron

---
+++ main.js
@@ -0,0 +1,106 @@
+const menuBtn = document.getElementById("menu-btn");
+const navLinks = document.getElementById("nav-links");
+const menuBtnIcon = menuBtn.querySelector("i");
+
+menuBtn.addEventListener("click", () => {
+  navLinks.classList.toggle("open");
+
+  const isOpen = navLinks.classList.contains("open");
+  menuBtnIcon.setAttribute("class", isOpen ? "ri-close-line" : "ri-menu-line");
+});
+
+navLinks.addEventListener("click", () => {
+  navLinks.classList.remove("open");
+  menuBtnIcon.setAttribute("class", "ri-menu-line");
+});
+
+const scrollRevealOption = {
+  distance: "50px",
+  origin: "bottom",
+  duration: 1000,
+};
+
+// header container
+ScrollReveal().reveal(".header__container p", {
+  ...scrollRevealOption,
+});
+
+ScrollReveal().reveal(".header__container h1", {
+  ...scrollRevealOption,
+  delay: 500,
+});
+
+// about container
+ScrollReveal().reveal(".about__image img", {
+  ...scrollRevealOption,
+  origin: "left",
+});
+
+ScrollReveal().reveal(".about__content .section__subheader", {
+  ...scrollRevealOption,
+  delay: 500,
+});
+
+ScrollReveal().reveal(".about__content .section__header", {
+  ...scrollRevealOption,
+  delay: 1000,
+});
+
+ScrollReveal().reveal(".about__content .section__description", {
+  ...scrollRevealOption,
+  delay: 1500,
+});
+
+ScrollReveal().reveal(".about__btn", {
+  ...scrollRevealOption,
+  delay: 2000,
+});
+
+// room container
+ScrollReveal().reveal(".room__card", {
+  ...scrollRevealOption,
+  interval: 500,
+});
+
+// service container
+ScrollReveal().reveal(".service__list li", {
+  ...scrollRevealOption,
+  interval: 500,
+  origin: "right",
+});
+
+// experience container
+ScrollReveal().reveal(".experience__container p", {
+  ...scrollRevealOption,
+  interval: 500,
+  origin: "right",
+});
+
+// experience rotation image
+document.addEventListener("DOMContentLoaded", function () {
+
+  const images = [
+    "assets/experience.jpg",
+    "assets/experience2.jpg",
+    "assets/experience3.jpg"
+  ];
+
+  let index = 0;
+  const section = document.querySelector(".experience");
+
+  function changeBackground() {
+    // Mueve hacia la derecha
+    section.style.backgroundPosition = "right";
+
+    setTimeout(() => {
+      section.style.backgroundImage = "url(" + images[index] + ")";
+      section.style.backgroundPosition = "center center";
+      index = (index + 1) % images.length;
+    }, 400);
+  }
+
+  changeBackground();
+  setInterval(changeBackground, 4000);
+
+});
+
